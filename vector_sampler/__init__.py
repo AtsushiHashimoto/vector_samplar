cade8264603a9d466795a2704b5a90b79d76fa83
Atsushi Hashimoto <ahasimoto@mm.media.kyoto-u.ac.jp>
2016-09-25 15:26:44 +0900

    fix all bugs for pip install. version 0.0.1->0.0.2

--- vector_sampler/__init__.py
+++ vector_sampler/__init__.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import vector_sampler
+from .vector_sampler import nsample
+from .vector_sampler import vec_sample
+from .vector_sampler import nvec_sample
+
 
 __author__  = 'AtsushiHashimoto'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __license__ = 'MIT'
