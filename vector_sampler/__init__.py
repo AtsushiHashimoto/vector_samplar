18b4a3963aca384258837637c3fbd223124448df
Atsushi Hashimoto <ahasimoto@mm.media.kyoto-u.ac.jp>
2016-09-25 14:57:36 +0900

    bug fix (import function names).

--- vector_sampler/__init__.py
+++ vector_sampler/__init__.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from vector_sampler import vsample
+from vector_sampler import nsample
+from vector_sampler import vec_sample
+from vector_sampler import nvec_sample
  
 __author__  = 'AtsushiHashimoto'
 __version__ = '0.0.1'
