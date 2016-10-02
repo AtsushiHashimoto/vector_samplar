057faa80a85e3207130ee6e5e3aa143544d2ee1d
Atsushi Hashimoto <ahasimoto@mm.media.kyoto-u.ac.jp>
2016-10-02 21:15:57 +0900

    remove __main__ function

--- vector_sampler/vector_sampler.py
+++ vector_sampler/vector_sampler.py
@@ -39,37 +39,3 @@ def nvec_sample(shape,dist_params):
             return function(*args,**kw)
         return _
     return _1
-        
-        
-'''if __name__ == '__main__':
-    import random
-    class_num = 2
-    vec_dim = 3
-    each_class_num = 4
-    sample_num = class_num * each_class_num
-    samples = np.zeros((sample_num,vec_dim))
-    labels = np.zeros(sample_num)
-
-    @nsample(vec_dim)
-    def ngauss(mean, sigma):
-        return random.gauss(mean,sigma)            
-
-    offset = 0
-    for c in range(class_num):        
-        class_seeds = ngauss(0, 10)
-        class_sigmas = ngauss(3, 1)
-        @nvec_sample((each_class_num,vec_dim), [[x , y] for (x, y) in zip(class_seeds,class_sigmas)])
-        def my_gauss(param):
-            return random.gauss(param[0],param[1])
-        _samples = my_gauss()
-        for i in range(each_class_num):
-            samples[i+offset] = _samples[i]
-            labels[i+offset] = c
-        offset = offset + each_class_num
-
-    print(samples)            
-    print(labels)                              
-        
-
-    
-    
